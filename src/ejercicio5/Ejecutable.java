d298c620a657bcfab5cea621ffb0c53716361c83
JuanManuelDS <juanmadesenzi@gmail.com>
2022-10-11 20:19:06 +0200

    comentarios

--- src/ejercicio5/Ejecutable.java
+++ src/ejercicio5/Ejecutable.java
@@ -1,32 +1,33 @@
 package ejercicio5;
 
 public class Ejecutable {
-	
+
 	private Password[] passwords;
-	private boolean [] fuertes;
+	private boolean[] fuertes;
 	private int largo;
-	
-	//constructor
+
+	// constructor
 	public Ejecutable(int largo) {
 		this.largo = largo;
 		passwords = new Password[largo];
 	}
-	
-	//relleno el array con el largo pasado por parámetro
+
+	// relleno el array con el largo pasado por parámetro
 	public void rellenarArray(int longitud) {
 		for (int i = 0; i < passwords.length; i++) {
 			passwords[i] = new Password(longitud);
 		}
 	}
-	
-	//setea el atributo fuertes con un array de booleanos, que serán true o false dependiendo si son contraseñas fuertes o no
-	public void sonFuertes () {
+
+	// setea el atributo fuertes con un array de booleanos, que serán true o false
+	// dependiendo si son contraseñas fuertes o no
+	public void sonFuertes() {
 		fuertes = new boolean[passwords.length];
 		for (int i = 0; i < passwords.length; i++) {
 			fuertes[i] = passwords[i].esFuerte();
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		sonFuertes();
@@ -36,5 +37,5 @@ public class Ejecutable {
 		}
 		return aux;
 	}
-	
+
 }
